81cef606ffd5f31fcba0cf0654d13f61ad95e812
Matteo Golin <matteo.golin@gmail.com>
2022-10-16 22:11:52 -0400

    Created a component to handle the altitude graph, which is displayed on the home page. Basic real-time update functionality is implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import "./App.css";
 
 // Hooks
 import { useWebsocket } from "./hooks/useWebsocket";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Components
 import { Routes, Route, json } from "react-router-dom";
 import Navbar from "./components/nav/Navbar";
 import PageLink from "./components/nav/PageLink";
+
+// Utilities
+import {
+  read_telemetry,
+  write_telemetry,
+  set_resolution,
+  clear_telemetry,
+} from "./utils/storage";
 
 // Pages
 import Home from "./pages/Home";
@@ -15,19 +23,25 @@ import Home from "./pages/Home";
 function App() {
   // Websocket data
   const [data, setData] = useWebsocket("ws://localhost:33845/websocket");
-  console.log(data);
 
   // Unpack and distribute data
-  var telemetry_data = null;
   var version = "X.X.X";
   var organization = "CU InSpace";
   var status = null;
+
   if (data) {
-    telemetry_data = data.telemetry_data;
     version = data.version;
     organization = data.org;
     status = data.status;
   }
+
+  // Write the newest data to local storage so it can be accessed anywhere
+  set_resolution(10);
+  useEffect(() => {
+    if (data) {
+      write_telemetry(data.telemetry_data);
+    }
+  }, [data]);
 
   // Current page
   const [currentPage, setCurrentPage] = useState("/"); // To Do: Have the current page link highlighted red
@@ -38,7 +52,7 @@ function App() {
         <PageLink to="/">Home</PageLink>
       </Navbar>
       <Routes>
-        <Route path="/" element={<Home data={telemetry_data} />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,15 @@
 import React from "react";
 import "./Home.css";
-import ReactEcharts from "echarts-for-react";
 
-export default function Home({ data }) {
-  // Unpack data
-  var altitude = null;
-  var acceleration = null;
-  var position = null;
+// Components
+import AltitudeGraph from "../components/main-dash/AltitudeGraph";
 
-  if (data) {
-    altitude = data.altitude_data;
-    acceleration = data.acceleration_data;
-    position = data.position_data;
-  }
-
-  const option = {
-    xAxis: {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: "bar",
-      },
-    ],
-  };
-
+export default function Home() {
   return (
     <main id="home">
       <h1>Main Dashboard</h1>
       <section id="graphs">
-        <ReactEcharts className="card" option={option} />
-        <ReactEcharts className="card" option={option} />
-        <ReactEcharts className="card" option={option} />
-        <ReactEcharts className="card" option={option} />
-        <ReactEcharts className="card" option={option} />
+        <AltitudeGraph className="card" />
       </section>
     </main>
   );
