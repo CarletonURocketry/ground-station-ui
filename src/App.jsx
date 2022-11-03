6a9b8c510514f77aa6ecdf41d5208b808e0b1204
Hamnah <hamnahq25@gmail.com>
2022-11-03 18:33:26 -0400

    Merged

--- src/App.jsx
+++ src/App.jsx
@@ -64,3 +64,64 @@ function App() {
 }
 
 export default App;
+import "./App.css";
+
+// Hooks
+import { useWebsocket } from "./hooks/useWebsocket";
+import { useEffect, useState } from "react";
+
+// Components
+import { Routes, Route, json } from "react-router-dom";
+import Navbar from "./components/nav/Navbar";
+import PageLink from "./components/nav/PageLink";
+
+// Utilities
+import {
+  read_telemetry,
+  write_telemetry,
+  set_resolution,
+  clear_telemetry,
+} from "./utils/storage";
+
+// Pages
+import Home from "./pages/Home";
+
+function App() {
+  // Websocket data
+  const data = useWebsocket("ws://localhost:33845/websocket");
+
+  // Unpack and distribute data
+  var version = "X.X.X";
+  var organization = "CU InSpace";
+  var status = null;
+
+  if (data) {
+    version = data.version;
+    organization = data.org;
+    status = data.status;
+  }
+
+  // Write the newest data to local storage so it can be accessed anywhere
+  set_resolution(10);
+  useEffect(() => {
+    if (data) {
+      write_telemetry(data.telemetry_data);
+    }
+  }, [data]);
+
+  // Current page
+  const [currentPage, setCurrentPage] = useState("/"); // To Do: Have the current page link highlighted red
+
+  return (
+    <div id="App">
+      <Navbar version={version} org={organization} status={status}>
+        <PageLink to="/">Home</PageLink>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
