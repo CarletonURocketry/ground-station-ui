f0ca1f9aac712ed99d169601a985e1b1436448e7
Matteo Golin <matteo.golin@gmail.com>
2022-11-04 18:04:14 -0400

    Added generic component for main dashboard graphs.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import "./Home.css";
 
 // Components
-import AltitudeGraph from "../components/main-dash/AltitudeGraph";
+import DashboardGraph from "../components/main-dash/DashboardGraph";
 
 export default function Home() {
+  // Altitude data callbacks
+  const get_altitude_x = (data) => {
+    return data.map((packet) => packet.altitude.mission_time / 1000);
+  };
+  const get_altitude_y = (data) => {
+    return data.map((packet) => packet.altitude.altitude.metres);
+  };
+
   return (
     <main id="home">
       <h1>Main Dashboard</h1>
       <section id="graphs">
-        <AltitudeGraph className="card" />
+        <DashboardGraph
+          title="Altitude"
+          x_title="Time (s)"
+          y_title="Altitude (m)"
+          x_cb={get_altitude_x}
+          y_cb={get_altitude_y}
+        />
       </section>
     </main>
   );
