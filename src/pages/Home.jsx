4bef20139806c33b5ff4ed5869e2fc1fc48bdb88
Hamnah <hamnahq25@gmail.com>
2022-11-04 18:24:57 -0400

    fix the merge files as there was repeats

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,19 +16,4 @@ export default function Home() {
     </main>
   );
 }
-import React from "react";
-import "./Home.css";
 
-// Components
-import AltitudeGraph from "../components/main-dash/AltitudeGraph";
-
-export default function Home() {
-  return (
-    <main id="home">
-      <h1>Main Dashboard</h1>
-      <section id="graphs">
-        <AltitudeGraph className="card" />
-      </section>
-    </main>
-  );
-}
