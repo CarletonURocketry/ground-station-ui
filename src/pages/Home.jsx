21040a5555bc798c6db3fa426b1cb71cfb9277fa
Matteo Golin <matteo.golin@gmail.com>
2022-11-04 18:08:36 -0400

    Added pressure graph to main dash.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,16 @@ import DashboardGraph from "../components/main-dash/DashboardGraph";
 
 export default function Home() {
   // Altitude data callbacks
-  const get_altitude_x = (data) => {
+  const get_altitude_mission_time = (data) => {
     return data.map((packet) => packet.altitude.mission_time / 1000);
   };
   const get_altitude_y = (data) => {
     return data.map((packet) => packet.altitude.altitude.metres);
+  };
+
+  // Pressure data callbacks
+  const get_pressure_y = (data) => {
+    return data.map((packet) => packet.altitude.pressure.kilopascals);
   };
 
   return (
@@ -21,8 +26,15 @@ export default function Home() {
           title="Altitude"
           x_title="Time (s)"
           y_title="Altitude (m)"
-          x_cb={get_altitude_x}
+          x_cb={get_altitude_mission_time}
           y_cb={get_altitude_y}
+        />
+        <DashboardGraph
+          title="Pressure"
+          x_title="Time (s)"
+          y_title="Pressure (kPa)"
+          x_cb={get_altitude_mission_time}
+          y_cb={get_pressure_y}
         />
       </section>
     </main>
