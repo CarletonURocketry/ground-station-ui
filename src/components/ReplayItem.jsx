5295dca60ed8b91861f9d6211ba934e4bf69fb19
Matteo Golin <matteo.golin@gmail.com>
2022-11-19 18:39:25 -0500

    Replaying of recordings now triggers a wipe of historical data in order to prevent data mixing on the graphs between mission.

--- src/components/ReplayItem.jsx
+++ src/components/ReplayItem.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./ReplayItem.css";
 
+import { clear_telemetry } from "../utils/storage";
+
 export default function ReplayItem({ name, websocketRef }) {
   const command = (event) => {
     websocketRef.current.send(`telemetry replay play ${name}`);
+    clear_telemetry(); // All data should be wiped because a new mission is starting
   };
 
   return (
