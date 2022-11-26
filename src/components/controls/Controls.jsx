96ea45acc45ab46072bbc2aaac4da2b2971645e9
Hamnah <hamnahq25@gmail.com>
2022-11-26 15:51:51 -0500

    Made code efficient removed unnecessary imports

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -1,108 +1,64 @@
 import React from "react";
 import "./Controls.css";
-
 import {useState} from "react";
-
-// Components
-//import MissionTimer from "./MissionTimer";
-
-// Utils
-import { clear_telemetry } from "../../utils/storage";
-
-/*
-telemetry replay speed <multiplier>
-telemetry replay play <mission>
-telemetry replay play
-telemetry replay pause
-telemetry replay stop 
-*/
 
 
 export default function Controls({websocketRef}){
 
+    const [speed, setSpeed] = useState(1); // Normal speed by default
 
-
-    const [pause, setPause] = useState(false);
-    const [play, setPlay] = useState(true);
-    const [fast, setFast] = useState(false);
-    const [slow, setSlow] = useState(false);
-
-
-        
-
-
-    function sentCommand(input){
-
-        switch (input) {
-            case "play":
-                const command = (event) => {
-                    websocketRef.current.send(`telemetry replay play`);
-                }; 
-                return command;
-            
-            case "pause":
-                const command1 = (event) => {
-                    websocketRef.current.send(`telemetry replay pause`);
-                }; 
-                return command1;
-            
-            case "fast": 
-                const command2 = (event) => {
-                    websocketRef.current.send(`telemetry replay speed 2`);
-                }; 
-                return command2;
-            
-            case "slow":
-                const command3 = (event) => {
-                    websocketRef.current.send(`telemetry replay speed 0.5`);
-                }; 
-                return command3;
-                
-
+    const fast_forward = (event) => {
+        if (speed <6){
+            setSpeed((prevSpeed) => prevSpeed * 2);
         }
-    }
+        websocketRef.current.send(`telemetry replay speed ${speed}`)
+        console.log(speed);
+    };
 
-        
+    const slow_forward = (event) => {
+        setSpeed((prevSpeed) => prevSpeed / 2);
+        websocketRef.current.send(`telemetry replay speed ${speed}`)
+    };
+
+    const play = (event) => {
+        websocketRef.current.send(`telemetry replay play`);
+    };
+
+    const pause = (event) => {
+        websocketRef.current.send(`telemetry replay pause`);
+    };
+
 
     return(
         
         <div className="buttons">
-
         <img
         src={require("../../assets/backward_button.png")}
         alt="backward"
-        onClick={sentCommand("slow")}
+        onClick={slow_forward}
         className="backward"
         />
 
         <img
         src={require("../../assets/play_button.png")}
         alt="play"
-        onClick={sentCommand("play")}
+        onClick={play}
         className="play"
         />
 
         <img
         src={require("../../assets/pause_button.png")}
         alt="pause"
-        onClick={sentCommand("pause")}
+        onClick={pause}
         className="pause"
         />
 
         <img
         src={require("../../assets/foward_button.png")}
         alt="foward"
-        onClick={sentCommand("fast")}
+        onClick={fast_forward}
         className="foward"
         />
-
-        
-
-        
-
-
-
-
 
         </div>
        
