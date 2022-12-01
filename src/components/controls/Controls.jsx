3ecdcc322aadf50c0b425291a9dcafc522550033
Matteo Golin <matteo.golin@gmail.com>
2022-12-01 13:35:40 -0500

    Control bar CSS cleaned up, displays current replay speed now. Shift + C to toggle control bar view

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -1,66 +1,63 @@
 import React from "react";
 import "./Controls.css";
-import {useState} from "react";
 
-//react components of svg files
+// Hooks
+import { useState } from "react";
+import { useKey } from "../../hooks/useKey";
+
+// SVG components
 import { ReactComponent as Play } from "../../assets/Play.svg";
 import { ReactComponent as Pause } from "../../assets/Pause.svg";
 import { ReactComponent as Forward } from "../../assets/Forward.svg";
 
+export default function Controls({ websocketRef }) {
+  const minSpeed = 0.25; // Minimum speed
+  const maxSpeed = 4; // Maximum speed
+  const [speed, setSpeed] = useState(1); // Tracks replay speed; normal speed of 1 by default
 
-export default function Controls({websocketRef}){
+  // Sends the speed command to fast forward
+  const fast_forward = () => {
+    if (speed < maxSpeed) {
+      setSpeed((prevSpeed) => prevSpeed * 2);
+    }
+    websocketRef.current.send(`telemetry replay speed ${speed}`);
+  };
 
-    const [speed, setSpeed] = useState(1); // Normal speed by default
+  // Sends the speed command to slow forward
+  const slow_forward = () => {
+    if (speed > minSpeed) {
+      setSpeed((prevSpeed) => prevSpeed / 2);
+    }
+    websocketRef.current.send(`telemetry replay speed ${speed}`);
+  };
 
-    //Sends the speed command to fast forward 
-    const fast_forward = (event) => {
-        //sets the maximum speed to 6
-        if (speed <4){
-            setSpeed((prevSpeed) => prevSpeed * 2);
-        }
-        websocketRef.current.send(`telemetry replay speed ${speed}`)
-    };
+  // Sends the play command
+  const play = () => {
+    websocketRef.current.send(`telemetry replay play`);
+  };
 
-    //sends the speed command to slow forward
-    const slow_forward = (event) => {
-        //Decreases the current speed by a factor of 2
-        if (speed >0.25){
-            setSpeed((prevSpeed) => prevSpeed / 2);
-        }
-        websocketRef.current.send(`telemetry replay speed ${speed}`)
-    };
+  // Sends the pause command
+  const pause = (event) => {
+    websocketRef.current.send(`telemetry replay pause`);
+  };
 
-    //sends the play command
-    const play = (event) => {
-        websocketRef.current.send(`telemetry replay play`);
-    };
+  // When Shift + C is pressed, toggle controls
+  const [hideControls, setHideControls] = useState(false);
+  useKey("KeyC", "shift", () => {
+    setHideControls((prevState) => !prevState);
+  });
 
-    //send the pause command
-    const pause = (event) => {
-        websocketRef.current.send(`telemetry replay pause`);
-    };
-
-    return(
-        
-        <div className="buttons">
-    
-        <Forward id = "backward"
+  return (
+    <div className={hideControls ? "buttons hide-controls" : "buttons"}>
+      <Forward
+        id="backward"
+        style={{ transform: "rotate(180deg)" }} // Flip the forward button to make it point backward
         onClick={slow_forward}
-        />
-
-        <Play id = "play"
-        onClick={play}
-        />
-
-        <Pause id = "pause"
-        onClick={pause}
-        />
-
-        <Forward id = "forward"
-        onClick={fast_forward}
-        />
-
-        </div> 
-    );
-    
+      />
+      <Play id="play" onClick={play} />
+      <p>{speed}X</p>
+      <Pause id="pause" onClick={pause} />
+      <Forward id="forward" onClick={fast_forward} />
+    </div>
+  );
 }
