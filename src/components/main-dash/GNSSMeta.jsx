dbe3fa2552b38d8db8ee0fb2ee24c8ddc2bf733c
Matteo Golin <matteo.golin@gmail.com>
2022-12-01 14:13:34 -0500

    GNSS data is displayed nicely now

--- src/components/main-dash/GNSSMeta.jsx
+++ src/components/main-dash/GNSSMeta.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { useStorage } from "../../hooks/useStorage";
 import "./GNSSMeta.css";
+
+// Hooks
+import { useStorage } from "../../hooks/useStorage";
+
+// Components
+import Satellite from "./Satellite";
 
 export default function GNSSMeta({ className }) {
   const get_metadata = (data) => {
@@ -11,6 +16,7 @@ export default function GNSSMeta({ className }) {
     return;
   };
 
+  // Satellites in use
   const [metadata, y] = useStorage(get_metadata, do_nothing);
   console.log(metadata);
   const num_glonass = metadata.glonass_sats_in_use
@@ -20,18 +26,31 @@ export default function GNSSMeta({ className }) {
     ? metadata.glonass_sats_in_use.length
     : "Unknown";
 
+  // Satellites in view
+  const in_view = metadata.sats_in_view.map((sat) => (
+    <Satellite
+      type={sat.type}
+      azimuth={sat.azimuth}
+      snr={sat.snr}
+      elevation={sat.elevation}
+    />
+  ));
+
   return (
-    <div className={className}>
+    <div className={className + " gnss"}>
       <div className="metadata">
-        <h4>GNSS Metadata</h4>
-        <p>
-          <strong>Glonass in use: </strong>
-          {num_glonass}
-        </p>
-        <p>
-          <strong>GPS in use: </strong>
-          {num_gps}
-        </p>
+        <h4>Satellites in View</h4>
+        <div className="in-view">{in_view}</div>
+        <div className="in-use">
+          <p>
+            <strong>Glonass in use: </strong>
+            {num_glonass}
+          </p>
+          <p>
+            <strong>GPS in use: </strong>
+            {num_gps}
+          </p>
+        </div>
       </div>
     </div>
   );
