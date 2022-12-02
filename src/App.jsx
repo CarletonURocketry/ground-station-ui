b13ebe903f4d2ee4f1627c4066798c3f1d21c34e
Matteo Golin <matteo.golin@gmail.com>
2022-12-02 11:33:29 -0500

    Cleaned unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 // Hooks
 import { useWebsocket } from "./hooks/useWebsocket";
 import { useState } from "react";
-import { useKey } from "./hooks/useKey";
 
 // Components
 import { Routes, Route } from "react-router-dom";
@@ -11,25 +10,18 @@ import Navbar from "./components/nav/Navbar";
 import { NavLink } from "react-router-dom";
 import Controls from "./components/controls/Controls";
 
-
-// Utilities
-import { set_resolution } from "./utils/storage";
-
 // Pages
 import Home from "./pages/Home";
 import Replays from "./pages/Replays";
 
 function App() {
   // Websocket data
-  set_resolution(10); // Keep 10 historical points
   const [websocketRef, status] = useWebsocket(
     "ws://localhost:33845/websocket",
     false
   );
 
   // Current pageS
-  const [currentPage, setCurrentPage] = useState("/"); // To Do: Have the current page link highlighted red
-
   return (
     <div id="App">
       <Navbar version={status.version} org={status.org} status={status.status}>
@@ -46,10 +38,10 @@ function App() {
           Replays
         </NavLink>
       </Navbar>
-      
-      <Controls websocketRef={websocketRef}/>
 
-      <Routes> 
+      <Controls websocketRef={websocketRef} />
+
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route
           path="/replays"
