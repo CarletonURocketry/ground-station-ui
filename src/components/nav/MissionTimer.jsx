69bc0c0f426bd7257d666ff0e97d454f2e1911bd
Matteo Golin <matteo.golin@gmail.com>
2022-12-02 11:49:34 -0500

    Handles -1 mission time now with a display of 'Waiting...'

--- src/components/nav/MissionTimer.jsx
+++ src/components/nav/MissionTimer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 export default function MissionTimer({ mission_time }) {
   // Convert ms to HH:MM:SS
-  const mission_duration = new Date(mission_time).toISOString().slice(11, 19);
+  let mission_duration;
+
+  if (mission_time == -1) {
+    mission_duration = "Waiting...";
+  } else {
+    mission_duration = new Date(mission_time).toISOString().slice(11, 19);
+  }
 
   return (
     <div id="mission-timer">
