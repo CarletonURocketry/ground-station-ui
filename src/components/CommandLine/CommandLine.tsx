e2b64f670da4c8b41838192c0fda6461d6b976d8
bskdany <54674556+bskdany@users.noreply.github.com>
2024-09-22 19:13:13 -0400

    Added command history navigation with arrow keys

--- src/components/CommandLine/CommandLine.tsx
+++ src/components/CommandLine/CommandLine.tsx
@@ -5,6 +5,9 @@ interface CommandLineProps {
   sendCommand: (command: string) => void;
 }
 
+const commandHistory: string[] = [];
+let commandHistoryIndex = -1; //  -1 because the list is empty
+
 /**
  * CommandLine
  * @param {function} sendCommand - Function to send commands to the backend.
@@ -13,8 +16,26 @@ interface CommandLineProps {
 function CommandLine({ sendCommand }: CommandLineProps): JSX.Element {
   const [command, setCommand] = useState("");
 
+
+  const handleCommandHistoryNavigation = (key: string) => {
+    if (commandHistory.length === 0) {
+      return;
+    }
+
+    if (key === "ArrowUp" && commandHistoryIndex > 0) {
+      commandHistoryIndex -= 1;
+      setCommand(commandHistory[commandHistoryIndex]);
+    }
+    else if (key === "ArrowDown" && commandHistoryIndex < commandHistory.length - 1) {
+      commandHistoryIndex += 1;
+      setCommand(commandHistory[commandHistoryIndex]);
+    }
+  }
+
   const handleSendCommand = () => {
     if (command.trim()) {
+      commandHistory.push(command);
+      commandHistoryIndex = commandHistory.length;
       sendCommand(command);
       setCommand(""); // Clear the input after sending the command
     }
@@ -28,6 +49,7 @@ function CommandLine({ sendCommand }: CommandLineProps): JSX.Element {
         onChange={(e) => setCommand(e.target.value)}
         placeholder="Enter command"
         className="command-input"
+        onKeyDown={(e) => handleCommandHistoryNavigation(e.key)}
       />
       <button onClick={handleSendCommand} className="send-button">
         Send Command
