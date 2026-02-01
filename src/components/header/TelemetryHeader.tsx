ced1178589f32fe5908bef7e44000b951407d378
incogiscool <94598096+incogiscool@users.noreply.github.com>
2026-02-01 17:41:42 -0500

    Fix telem header for mobile

--- src/components/header/TelemetryHeader.tsx
+++ src/components/header/TelemetryHeader.tsx
@@ -13,31 +13,56 @@ function TelemetryHeader() {
   const { data, lastMissionTime } = useTelemetryStore();
 
   return (
-    <header className="rounded-lg bg-white border-b p-4">
-      <div className="relative flex flex-col md:flex-row md:items-center md:justify-between gap-4">
-        {/* Left: Logo and Status */}
-        <div className="flex items-center gap-6">
+    <header className="rounded-lg bg-white border-b p-2 sm:p-4">
+      {/* Mobile Layout */}
+      <div className="flex flex-col gap-3 md:hidden">
+        <div className="flex items-center justify-between">
           <img
             src={inspaceLogo}
             alt="CUInSpace Logo"
-            className="h-12 md:h-14"
+            className="h-10 sm:h-12"
           />
+          <TelemetryValue
+            label="STATUS"
+            value={getMissionStatus(data.flight_status)}
+          />
+          <TelemetryValue
+            label="MISSION TIME"
+            value={getMissionTime(lastMissionTime)}
+          />
+        </div>
+        <div className="grid grid-cols-4 gap-2 text-center">
+          <TelemetryValue
+            label="ALTITUDE"
+            value={getAltitude(data.altitude_sea_level)}
+          />
+          <TelemetryValue
+            label="APOGEE"
+            value={getApogee(data.altitude_sea_level)}
+          />
+          <TelemetryValue label="SPEED" value="No data" />
+          <TelemetryValue label="MAX SPEED" value="No data" />
+        </div>
+      </div>
+
+      {/* Desktop Layout */}
+      <div className="relative hidden md:flex md:items-center md:justify-between">
+        <div className="flex items-center gap-6">
+          <img src={inspaceLogo} alt="CUInSpace Logo" className="h-14" />
           <TelemetryValue
             label="STATUS"
             value={getMissionStatus(data.flight_status)}
           />
         </div>
 
-        {/* Center: Mission Time - Absolutely positioned to center of screen */}
-        <div className="md:absolute md:left-1/2 md:-translate-x-1/2 flex justify-center">
+        <div className="absolute left-1/2 -translate-x-1/2">
           <TelemetryValue
             label="MISSION TIME"
             value={getMissionTime(lastMissionTime)}
           />
         </div>
 
-        {/* Right: Altitude, Apogee, Speed, Max Speed */}
-        <div className="grid grid-cols-2 md:flex md:items-center gap-4 md:gap-6">
+        <div className="flex items-center gap-6">
           <TelemetryValue
             label="ALTITUDE"
             value={getAltitude(data.altitude_sea_level)}
